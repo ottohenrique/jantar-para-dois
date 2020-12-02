621bf94c1ae36d97fb6c1cb1f5005b044710289c
Otto Henrique <otto.henrique@emcasa.com>
2020-12-01 22:31:33 -0300

    fixes recipes controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,13 +1,29 @@
 require 'test_helper'
 
+module SignInHelper
+  def sign_in_as(user)
+    post sessions_login_path(username: user.username, password: "projetoconclusao")
+  end
+end
+ 
+class ActionDispatch::IntegrationTest
+  include SignInHelper
+end
+
 class RecipesControllerTest < ActionDispatch::IntegrationTest
+  fixtures :users
+
   test "should get index" do
-    get recipes_index_url
+    sign_in_as users(:john)
+
+    get recipes_path
     assert_response :success
   end
 
   test "should get show" do
-    get recipes_show_url
+    sign_in_as users(:john)
+
+    get recipes_path(123)
     assert_response :success
   end
 
